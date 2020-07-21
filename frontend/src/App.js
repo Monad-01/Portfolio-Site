645cdf2d2eaf62168a712aa2547cea8fa99cc6b5
Johnathan Pruitt <pruittj1998@gmail.com>
2020-07-21 00:23:38 -0500

    frontend stuff

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header class="header">Bottom Texter</header>
+    </div>
+  );
+}
+
+export default App;
